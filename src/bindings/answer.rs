d9a3ed6d85c2d9cd7f48f645d9caa5897143bd5f
Dmitrii Ubskii <18616863+dmitrii-ubskii@users.noreply.github.com>
2023-07-05 16:38:57 +0100

    Fix license headers

--- src/bindings/answer.rs
+++ src/bindings/answer.rs
@@ -3,7 +3,7 @@
  *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
- * d*const c_chaributed with this work for additional information
+ * distributed with this work for additional information
  * regarding copyright ownership.  The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
@@ -12,7 +12,7 @@
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
- * software d*const c_chaributed under the License is distributed on an
+ * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
  * KIND, either express or implied.  See the License for the
  * specific language governing permissions and limitations
